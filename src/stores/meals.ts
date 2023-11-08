1e2aa4c29311814c3e1de57e9532e61e37ac8f8c
szvitek <gy.szvitek@gmail.com>
2023-11-08 17:27:16 +0000

    feat(MealsByLetter): meals by letter page

--- src/stores/meals.ts
+++ src/stores/meals.ts
@@ -7,12 +7,28 @@ export const useMealStore = defineStore('meal', () => {
   const meal = ref<Meal | null>(null)
   const meals = ref<Meal[]>([])
   const searchedMeals = ref<Meal[]>([])
+  const mealsByLetter = ref<Meal[] | null>([])
+  const mealsByIngredients = ref<Meal[]>([])
 
   async function searchMeals(keyword: string) {
     const {
       data: { meals }
     } = await axiosClient.get(`search.php?s=${keyword}`)
     searchedMeals.value = meals
+  }
+
+  async function searchMealsByLetter(letter: string) {
+    const {
+      data: { meals }
+    } = await axiosClient.get(`search.php?f=${letter}`)
+    mealsByLetter.value = meals
+  }
+
+  async function searchMealsByIngredient(ingredient: string) {
+    const {
+      data: { meals }
+    } = await axiosClient.get(`filter.php?i=${ingredient}`)
+    mealsByIngredients.value = meals
   }
 
   async function getMealById(id: string) {
@@ -22,5 +38,15 @@ export const useMealStore = defineStore('meal', () => {
     meal.value = meals?.[0] || null
   }
 
-  return { meal, meals, searchedMeals, searchMeals, getMealById }
+  return {
+    meal,
+    meals,
+    searchedMeals,
+    mealsByLetter,
+    mealsByIngredients,
+    searchMeals,
+    searchMealsByLetter,
+    searchMealsByIngredient,
+    getMealById
+  }
 })
--- src/views/MealsByLetterView.vue
+++ src/views/MealsByLetterView.vue
@@ -1,5 +1,30 @@
 <script setup lang="ts">
+import MealItem from '@/components/MealItem.vue'
+import { useMealStore } from '@/stores/meals'
+import { storeToRefs } from 'pinia'
+import { ref, watch } from 'vue'
+import { useRoute } from 'vue-router'
+
 const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const store = useMealStore()
+const { mealsByLetter: meals } = storeToRefs(store)
+const route = useRoute()
+const { searchMealsByLetter } = store
+const isLoading = ref(false)
+
+watch(
+  () => route.params.letter,
+  async (newLetter) => {
+    isLoading.value = true
+    if (newLetter) {
+      await searchMealsByLetter(newLetter as string)
+    } else {
+      meals.value = null
+    }
+    isLoading.value = false
+  },
+  { immediate: true, deep: true }
+)
 </script>
 
 <template>
@@ -12,6 +37,21 @@ const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
       {{ letter }}
     </RouterLink>
   </div>
+
+  <div v-if="isLoading" class="animate-pulse h-full flex items-center justify-center">
+    <h1>Loading...</h1>
+  </div>
+  <template v-else>
+    <div
+      v-if="meals && meals?.length"
+      class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 p-8"
+    >
+      <MealItem v-for="meal of meals" :key="meal.idMeal" :meal="meal" />
+    </div>
+    <div v-else-if="route.params.letter" class="flex items-center justify-center flex-grow flex-1">
+      <h1>No results...</h1>
+    </div>
+  </template>
 </template>
 
 <style scoped></style>
